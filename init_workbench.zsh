130bcde33256129bd74d580f0b13c039cac2e0bb
Andy Park <andy@bigbearlabs.com>
2017-09-25 22:31:05 +0200

    split out from sketches/.

---
+++ init_workbench.zsh
@@ -0,0 +1,21 @@
+HISTSIZE=1000
+SAVEHIST=10000
+setopt HIST_SAVE_NO_DUPS INC_APPEND_HISTORY
+
+
+# HISTFILE=~/.zsh/dirhist/${PWD//\//@}  # STRATEGY centralise around user preferences. 
+# mkdir -p ~/.zsh/dirhist
+
+HISTFILE=${PWD}/.history  # STRATEGY store in work dir.
+
+
+chpwd() {
+  [[ $PWD = $OLDPWD ]] || fc -Pp ${PWD}/.history
+}
+
+
+
+# NOTE trade-offs on where the context save location strategies:
+# - under ~/.zsh/dirhist : won't track path changes. context layered on top of paths without reverse dependency.
+# - under project path (leaf) : will work with moves. paths are dirtied.
+# - under project path (root) : will work with root move. paths are somewhat leses dirtied. subpaths will be context-unaware.
